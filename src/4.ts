ad786484963e7ecd5946e10bcb22acc9c8f0cc7e
Serge-Moskalenko <skmos2019@outlook.com>
2023-12-10 15:00:09 +0000

    finish

--- src/4.ts
+++ src/4.ts
@@ -1,18 +1,64 @@
-// class key {
+// class Key {
 //     constructor( private signature:number=0){
+//         if(this.signature === 0){
+//             this.signature= Math.random()
+//         }
+//     };
 
+//    public getSignature():number{
+//         return this.signature
+//     };
+// };
+
+// class Person { 
+//     constructor( private key:any ){};
+
+//     getKey():number{
+        
+//        return this.key.signature; 
+//     };
+// };
+
+// type Door= false|true;
+
+//  abstract class House{
+//   public door:Door;
+//   public tenents:object[]=[];
+//   constructor(protected key){
+//     this.key=key;
+//   }
+
+//   public comeIn(person:object):void{
+//     if(this.door ){
+//         this.tenents.push(person)
+//     } 
+//   };
+
+//   public abstract OpenDoor(key:number):boolean;
+// };
+
+// class MyHouse extends House {
+//     constructor(public key){
+//         super(key)
 //     }
 
-// }
+//   public  OpenDoor(key){
+//         if(key === this.key.signature){
+//             return  this.door=true;
+//         } return this.door= false
+//     };
+// };
 
 // const key = new Key();
-
 // const house = new MyHouse(key);
 // const person = new Person(key);
 
-// house.openDoor(person.getKey());
-
+// house.OpenDoor(person.getKey());
 // house.comeIn(person);
 
+// console.log(key.getSignature());
+// console.log(person.getKey())
+// console.log(house.OpenDoor(person.getKey()))
+// console.log(house.door)
+// console.log(house.tenents)
 
-// export {};
