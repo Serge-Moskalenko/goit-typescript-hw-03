efb73127c8593b3a35a4e0c3464ea5e09c9c6ca9
Serge-Moskalenko <skmos2019@outlook.com>
2023-12-10 14:58:27 +0000

    finish

--- src/4.ts
+++ src/4.ts
@@ -1,11 +1,18 @@
-const key = new Key();
+// class key {
+//     constructor( private signature:number=0){
 
-const house = new MyHouse(key);
-const person = new Person(key);
+//     }
 
-house.openDoor(person.getKey());
+// }
 
-house.comeIn(person);
+// const key = new Key();
+
+// const house = new MyHouse(key);
+// const person = new Person(key);
+
+// house.openDoor(person.getKey());
+
+// house.comeIn(person);
 
 
-export {};
+// export {};
